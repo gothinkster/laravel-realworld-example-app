e62ee941ea8640ba5f224d2628f8e7e4a13fd513
Sandeesh <sandeesh.s@gmail.com>
2017-05-06 19:02:51 +0530

    Added base pending tests.

---
+++ tests/Feature/Api/ArticleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ArticleTest extends TestCase
+{
+    use DatabaseMigrations;
+}
---
+++ tests/Feature/Api/CommentTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CommentTest extends TestCase
+{
+    use DatabaseMigrations;
+}
