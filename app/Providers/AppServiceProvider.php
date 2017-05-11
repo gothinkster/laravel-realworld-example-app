f219b1efa119b549f8b27cc4bfbe3cd3e6673f23
Sandeesh <sandeesh.s@gmail.com>
2017-05-11 23:30:14 +0530

    Fix for "Specified key was too long error" on pre v5.7.7 of MySQL due to laravel's utf8mb4 default character set.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Schema;
 use Illuminate\Support\ServiceProvider;
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
