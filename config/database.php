7339a930dfaee1aac5aca266a508f03d55f4d678
sasensi <As353,13>
2018-09-12 11:24:32 +0200

    add mysql modes config

--- config/database.php
+++ config/database.php
@@ -52,6 +52,14 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'modes'  => [
+	            'ONLY_FULL_GROUP_BY',
+	            'STRICT_TRANS_TABLES',
+	            'NO_ZERO_IN_DATE',
+	            'NO_ZERO_DATE',
+	            'ERROR_FOR_DIVISION_BY_ZERO',
+	            'NO_ENGINE_SUBSTITUTION',
+            ],
         ],
 
         'pgsql' => [
