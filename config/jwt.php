c4b194d3db98c29ad395ba4c1cda19130b978bf7
iya30n <varmeziyaryasin@gmail.com>
2021-09-25 17:57:37 +0330

    fix jwt error

--- config/jwt.php
+++ config/jwt.php
@@ -275,7 +275,8 @@ return [
         |
         */
 
-        'jwt' => Tymon\JWTAuth\Providers\JWT\Lcobucci::class,
+        // 'jwt' => Tymon\JWTAuth\Providers\JWT\Lcobucci::class,
+        'jwt' => Tymon\JWTAuth\Providers\JWT\Namshi::class,
 
         /*
         |--------------------------------------------------------------------------
