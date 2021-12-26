aa06aadb4423c2f6b5823f807e7aedee0f33ceaa
Du Zhigang <689799+dulao5@users.noreply.github.com>
2021-12-27 00:54:06 +0900

    tidb port
    close #1

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('TIDB_PORT', '4000'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
